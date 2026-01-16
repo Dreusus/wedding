53e9c0dffb524bc64bb7d550624072d1180df8f0
Dreusus <pitakxdd@gmail.com>
2026-01-16 17:19:30 +0300

    ci: удалены служебные файлы react-scripts

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Body from '../Body/Body';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
