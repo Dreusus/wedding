441eec68c47452e9491af4fefba4a55c3d7b3b60
Dreusus <pitakxdd@gmail.com>
2026-01-18 16:13:38 +0300

    refactor: изменена разметка и стили Footer, обновлены пути к изображениям

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,56 +1,42 @@
-import { weddingConfig } from '../../config';
 import './Footer.scss';
 
 export const Footer = () => {
   return (
     <section className="footer-section" id="footer">
-      {/* Background image with overlay */}
-      <div
-        className="footer-background"
-        style={{
-          backgroundImage: `url(${weddingConfig.images.footer.background})`,
-        }}
-      >
-        <div className="footer-overlay"></div>
+      {/* Роза выезжает сверху */}
+      <div className="footer-roses-container">
+        <img
+          src="/images/roses.png"
+          alt=""
+          className="footer-roses animate-from-top"
+        />
       </div>
 
-      {/* Content */}
-      <div className="footer-content">
-        {/* Couple photo */}
-        <div className="footer-couple-container">
-          <img
-            src={weddingConfig.images.footer.couple}
-            alt={`${weddingConfig.groom.name} и ${weddingConfig.bride.name}`}
-            className="footer-couple-image"
-          />
-        </div>
+      {/* 1.svg выезжает из розы */}
+      <div className="footer-svg1-container">
+        <img
+          src="/images/footer-1.svg"
+          alt=""
+          className="footer-svg1 animate-from-rose"
+        />
+      </div>
 
-        {/* We're waiting text - using SVG */}
-        <div className="footer-text-container">
-          <img
-            src={weddingConfig.images.svg.footer}
-            alt="Ждём вас!"
-            className="footer-text-svg"
-          />
-        </div>
+      {/* 2.svg выезжает снизу */}
+      <div className="footer-svg2-container">
+        <img
+          src="/images/footer-2.svg"
+          alt=""
+          className="footer-svg2 animate-from-bottom"
+        />
+      </div>
 
-        {/* Couple photos side by side */}
-        <div className="footer-photos-row">
-          <div className="footer-photo-wrapper left">
-            <img
-              src={weddingConfig.groom.image}
-              alt={weddingConfig.groom.name}
-              className="footer-side-photo"
-            />
-          </div>
-          <div className="footer-photo-wrapper right">
-            <img
-              src={weddingConfig.bride.image}
-              alt={weddingConfig.bride.name}
-              className="footer-side-photo"
-            />
-          </div>
-        </div>
+      {/* End */}
+      <div className="footer-end-container">
+        <img
+          src="/images/end.png"
+          alt=""
+          className="footer-end-image"
+        />
       </div>
     </section>
   );
